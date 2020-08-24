287ff85d8ba747a17dcee568b766aaecf0b16e73
zanderhorstman <zanderhorstman@gmail.com>
2020-08-24 18:47:23 -0400

    commit2

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,18 @@ public class Main {
         boolean hasInsurance = false;
         double temp = 102.2;
         int numVisits = 5;
+        double coPay = 35.26;
+        String nextAppointment = "Tuesday 3/10";
+        String contact = "(304)039-9384";/*
+            Not a real phone number*/
 
         System.out.println("Patient Details:");
         System.out.println("Name: " + patientName);
         System.out.println("Patient has insurance: " + hasInsurance);
         System.out.println("Body temperature: " + temp);
         System.out.println("Number of visits: " + numVisits);
+        System.out.println("Copay amount: $" + coPay);
+        System.out.println("Next appointment: " + nextAppointment);
+        System.out.println("Contact number on file: " + contact);
     }
 }
