65ef6f55fffc84d1264221122eb0f2b0d1f1c353
zanderhorstman <zanderhorstman@gmail.com>
2020-08-24 17:28:29 -0400

    Commit DO(WE)

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args){
+
+        String patientName = "Stacy Mason";
+        boolean hasInsurance = false;
+        double temp = 102.2;
+        int numVisits = 5;
+
+        System.out.println("Patient Details:");
+        System.out.println("Name: " + patientName);
+        System.out.println("Patient has insurance: " + hasInsurance);
+        System.out.println("Body temperature: " + temp);
+        System.out.println("Number of visits: " + numVisits);
+    }
+}
